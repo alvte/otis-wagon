5bb5e76a01f087d2512ba24ab0168387ae92d594
alban veaute <alban.veaute@gmail.com>
2023-11-27 18:00:45 +0100

    add controllers

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,45 @@
 class CardsController < ApplicationController
+  before_action :find, only: %i[show edit update destroy]
+
+  def index
+    @cards = Card.all
+  end
+
+  # only for admin users
+  def show; end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+    @card.user = current_user
+    if @card.save
+      redirect_to card_path(@card)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    @card.update(card_params)
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to root_path status: :see_other
+  end
+
+  private
+
+  def find
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:title, :summary, :content, :additional_resources, photos: [])
+  end
 end
