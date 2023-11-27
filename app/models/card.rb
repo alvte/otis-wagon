fdf8b821f953c3bb3fc42839aa9da107bd25babc
alban veaute <alban.veaute@gmail.com>
2023-11-27 18:04:22 +0100

    add models

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,5 @@
 class Card < ApplicationRecord
+  #has_many :photos
+  has_many :card_categories
+  has_many :categories, through: :card_categories
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  has_many :card_categories
+  has_many :product_categories
+  has_many :cards, through: :card_categories
+  has_many :products, through: :product_categories
 end
