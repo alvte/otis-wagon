fcae88e990f38f35f5ca86f2ca47459b90d18af1
victor bostaetter <victor.bostaetter@docent-art.com>
2023-11-28 18:01:08 +0100

    draft_create_chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -10,4 +10,26 @@ class ChatroomsController < ApplicationController
     @user = User.find(params[:user_id])
     @chatrooms = @user.chatrooms
   end
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.user = current_user
+    raise
+
+    if @chatroom.save
+      redirect_to user_chatrooms_path(current_user), notice: "Chatroom created successfully."
+    else
+      render :new
+    end
+  end
+
+  private
+  def chatroom_params
+    params.require(:chatroom).permit(:name, :user_id)
+  end
+
 end
