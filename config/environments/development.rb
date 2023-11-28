6163de5ddca5494e2a2f73f56499e305b074876f
Anahita Hekmat <hekmatana@gmail.com>
2023-11-28 13:18:21 +0100

    pwa setting done-404

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,8 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.hosts.clear
+
   config.enable_reloading = true
 
   # Do not eager load code on boot.
