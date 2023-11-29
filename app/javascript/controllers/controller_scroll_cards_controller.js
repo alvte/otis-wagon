65933e87bd944f6eb9009bbd8a06affeac2671ba
alban veaute <alban.veaute@gmail.com>
2023-11-29 11:52:03 +0100

    add js to overlap cards

--- app/javascript/controllers/controller_scroll_cards_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="controller-scroll-cards"
-export default class extends Controller {
-  connect() {
-    console.log("Hello world")
-  }
-}
---
+++ app/javascript/controllers/scroll_cards_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["card"];
+  static values = { counter: Number };
+
+  connect() {
+    this.cardTargets.forEach(element => {
+      const position = element.dataset.scrollCardsValue * 250;
+      element.style.bottom = `${position}px`;
+    });
+    }
+
+  push_down(event) {
+    console.log("hello world");
+  }
+  }
