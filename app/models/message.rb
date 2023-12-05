b26d5c3e9144e97062a1ba2b9ad2d44645c34844
victor bostaetter <victor.bostaetter@docent-art.com>
2023-12-05 16:52:49 +0100

    working render messages for marketplace

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,11 @@
+require 'JSON'
+
 class Message < ApplicationRecord
   belongs_to :chatroom
   belongs_to :user, optional: true
   belongs_to :professional, optional: true
+
+  def parse_content
+    JSON.parse(self.content)
+  end
 end
